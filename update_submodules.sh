e4158b0d58f0dc4eeef35825e59eb982c650027b
mikespub <mikespub@users.noreply.github.com>
2018-05-10 23:14:02 +0200

    add K5-InterProject-Demo

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #
+#git clone --recurse-submodules https://github.com/mikespub-org/allthingsclowd.git
+#
+#git submodule add https://github.com/allthingsclowd/K5-InterProject-Demo.git
 git submodule update --remote --merge
 git config status.submodulesummary 1
 git status
